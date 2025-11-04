76a5091933acc64ad2476905d4fad3bbdf7cfe2b
aleeazeem <aleeazeem@gmail.com>
2025-11-04 13:08:20 -0800

    Edit cred vars

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -42,6 +42,7 @@ def apply_bow(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: i
         test_df = pd.DataFrame(X_test_bow.toarray())
         test_df['label'] = y_test
 
+        logging.info('Dumping to vectorizer.pkl file')
         pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb'))
         logging.info('Bag of Words applied and data transformed')
 
